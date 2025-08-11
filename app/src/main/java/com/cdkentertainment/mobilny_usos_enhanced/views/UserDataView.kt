8da2edf092b5b623e4c2c8d19e3d579aa769292f
AleksyPrawilow <a5.pravilov@gmail.com>
2025-08-12 01:30:14 +0200

    Fixed theme switch, unified the border width between the fab and user profile picture, added easing to the fab sub buttons

--- app/src/main/java/com/cdkentertainment/mobilny_usos_enhanced/views/UserDataView.kt
+++ app/src/main/java/com/cdkentertainment/mobilny_usos_enhanced/views/UserDataView.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,6 +57,7 @@ fun UserDataView(homePageState: HomePageViewModel) {
     ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(paddingSize.dp)
         ) {
             Card(
@@ -68,12 +70,13 @@ fun UserDataView(homePageState: HomePageViewModel) {
                 shape = RoundedCornerShape(50.dp),
                 modifier = Modifier
                     .size(72.dp)
-                    .border(3.dp, UISingleton.color3.primaryColor, shape = RoundedCornerShape(50.dp))
+                    .border(5.dp, UISingleton.color4.primaryColor, shape = RoundedCornerShape(50.dp))
             ) {
                 // TODO: Add the image load
                 Icon(
                     imageVector = Icons.Rounded.Person,
                     contentDescription = "Person",
+                    tint = UISingleton.color4.primaryColor,
                     modifier = Modifier
                         .fillMaxSize()
                         .graphicsLayer(
