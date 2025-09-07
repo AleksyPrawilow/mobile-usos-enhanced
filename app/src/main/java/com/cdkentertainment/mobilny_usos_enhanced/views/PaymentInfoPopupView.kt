fa55f55da9f5dc065f3b903545531186936dba91
AleksyPrawilow <a5.pravilov@gmail.com>
2025-09-07 23:12:05 +0200

    Popup for payments

---
+++ app/src/main/java/com/cdkentertainment/mobilny_usos_enhanced/views/PaymentInfoPopupView.kt
@@ -0,0 +1,113 @@
+package com.cdkentertainment.mobilny_usos_enhanced.views
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.cdkentertainment.mobilny_usos_enhanced.UISingleton
+import com.cdkentertainment.mobilny_usos_enhanced.models.Payment
+
+@Composable
+fun PaymentInfoPopupView(
+    data: Payment,
+    onDismissRequest: () -> Unit
+) {
+    val clipboard = LocalClipboardManager.current
+    val context: Context = LocalContext.current
+    val toast: Toast = Toast.makeText(context, "Skopiowano do schowka", Toast.LENGTH_SHORT)
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
+    ) {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                //.fillMaxSize()
+                .fillMaxWidth()
+                .shadow(10.dp, shape = RoundedCornerShape(UISingleton.uiElementsCornerRadius.dp))
+                .background(UISingleton.color2, RoundedCornerShape(UISingleton.uiElementsCornerRadius.dp))
+                .border(5.dp, UISingleton.color1, RoundedCornerShape(UISingleton.uiElementsCornerRadius.dp))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Spacer(modifier = Modifier.height(48.dp))
+                Text(
+                    text = data.description.pl,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = UISingleton.textColor1,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 6.dp)
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                PaymentInfoView(data)
+                if (data.state == "unpaid") {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 12.dp, end = 12.dp, top = 12.dp)
+                            .clip(RoundedCornerShape(UISingleton.uiElementsCornerRadius.dp))
+                            .background(UISingleton.color1)
+                            .clickable(onClick = {
+                                clipboard.setText(AnnotatedString(data.account_number))
+                                toast.show()
+                            })
+                            .padding(12.dp)
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            Text(
+                                text = "Konto bankowe",
+                                style = MaterialTheme.typography.titleLarge,
+                                color = UISingleton.textColor1
+                            )
+                            HorizontalDivider(
+                                thickness = 5.dp,
+                                color = UISingleton.textColor2,
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(UISingleton.uiElementsCornerRadius.dp))
+                            )
+                            Text(
+                                text = data.account_number,
+                                style = MaterialTheme.typography.titleMedium,
+                                color = UISingleton.textColor1
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+            }
+            DismissPopupButtonView(onDismissRequest = onDismissRequest, modifier = Modifier.align(Alignment.TopEnd))
+        }
+    }
+}
--- app/src/main/java/com/cdkentertainment/mobilny_usos_enhanced/views/PaymentView.kt
+++ app/src/main/java/com/cdkentertainment/mobilny_usos_enhanced/views/PaymentView.kt
@@ -1,18 +1,29 @@
 package com.cdkentertainment.mobilny_usos_enhanced.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.text.font.FontWeight
@@ -26,6 +37,10 @@ import com.cdkentertainment.mobilny_usos_enhanced.models.Payment
 fun PaymentView(
     payment: Payment
 ) {
+    var showDetails: Boolean by rememberSaveable { mutableStateOf(false) }
+    if (showDetails) {
+        PaymentInfoPopupView(data = payment, onDismissRequest = { showDetails = false })
+    }
     Column(
         modifier = Modifier
             .drawBehind {
@@ -34,6 +49,10 @@ fun PaymentView(
                     cornerRadius = CornerRadius(UISingleton.uiElementsCornerRadius.dp.toPx(), UISingleton.uiElementsCornerRadius.dp.toPx())
                 )
             }
+            .clip(RoundedCornerShape(UISingleton.uiElementsCornerRadius.dp))
+            .clickable(onClick = {
+                showDetails = true
+            })
     ) {
         Card(
             colors = CardDefaults.cardColors(
@@ -52,14 +71,38 @@ fun PaymentView(
                 .fillMaxWidth()
                 .zIndex(1f)
         ) {
-            Text(
-                text = payment.description.pl,
-                style = MaterialTheme.typography.titleLarge,
-                color = UISingleton.textColor1,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding(12.dp)
-            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(0.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            ) {
+                Text(
+                    text = payment.description.pl,
+                    style = MaterialTheme.typography.titleLarge,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = UISingleton.textColor1,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 6.dp)
+                )
+                Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.KeyboardArrowRight,
+                    contentDescription = "More",
+                    tint = UISingleton.textColor1,
+                    modifier = Modifier
+                )
+            }
+//            Text(
+//                text = payment.description.pl,
+//                style = MaterialTheme.typography.titleLarge,
+//                color = UISingleton.textColor1,
+//                maxLines = 2,
+//                overflow = TextOverflow.Ellipsis,
+//                modifier = Modifier.padding(12.dp)
+//            )
         }
         Row(
             modifier = Modifier.fillMaxWidth()
@@ -82,7 +125,7 @@ fun PaymentView(
                     .padding(top = 4.dp, bottom = 4.dp, start = 12.dp, end = 12.dp)
             )
             Text(
-                text = "${"%.2f".format(payment.total_amount)} zł",
+                text = "${"%.2f".format(if (payment.state == "unpaid") payment.total_amount - payment.saldo_amount else payment.total_amount)} zł",
                 style = MaterialTheme.typography.titleMedium,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
